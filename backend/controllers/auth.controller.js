ff1f41982830533fdfc95d117614fba2388fff24
Zenn <149383000+Cenx-bash@users.noreply.github.com>
2026-08-27 19:50:16 +0800

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -17,7 +17,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
-const pool = require("../config/database");
 
 const {
     sendOTPEmail,
