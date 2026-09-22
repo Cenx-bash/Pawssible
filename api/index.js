7c5c61dfedc1134d24bedb5dd30a1d0045db91ae
Karl Ancel <bnom.karlanceldimabayao@gmail.com>
2026-09-22 22:40:36 +0800

    Fix Vercel frontend and API deployment

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
 const app = require("../backend/server");
 
-app.use((req, res, next) => {
-    console.log("VERCEL REQUEST:", req.method, req.originalUrl);
-    next();
-});
-
 module.exports = app;
